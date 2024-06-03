95742921b4d87de502069e36f9ff5d9fc74b1665
mustika0806 <MY_NAME@example.com>
2024-06-03 17:27:34 +0700

    Feat: Database untuk tampilin buku

--- app/Http/Controllers/MainpageController.php
+++ app/Http/Controllers/MainpageController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Buku;
+use App\Models\Kategori;
 
 class MainpageController extends Controller
 {
@@ -16,10 +18,51 @@ class MainpageController extends Controller
         return view('history');
     }
 
-    public function kategori()
+    public function kategori(Request $request)
     {
-        return view('kategori');
+        $BukuFiksi = Buku::join('kategori', 'buku.Kategori_ID', '=', 'kategori.Kategori_ID')
+            ->where('kategori.Fiksi', 'Ya')
+            ->where('kategori.NonFiksi', 'Tidak')
+            ->select('buku.*', 'kategori.*')
+            ->get();
+        $BukuNonFiksi = Buku::join('kategori', 'buku.Kategori_ID', '=', 'kategori.Kategori_ID')
+            ->where('kategori.Fiksi', 'Tidak')
+            ->where('kategori.NonFiksi', 'Ya')
+            ->select('buku.*', 'kategori.*')
+            ->get();
+        $BukuAgama = Buku::join('kategori', 'buku.Kategori_ID', '=', 'kategori.Kategori_ID')
+            ->where('kategori.Agama', 'Islam')
+            ->select('buku.*', 'kategori.*')
+            ->get();
+        $BukuBudaya = Buku::join('kategori', 'buku.Kategori_ID', '=', 'kategori.Kategori_ID')
+            ->where('kategori.Budaya', 'Indonesia')
+            ->select('buku.*', 'kategori.*')
+            ->get();
+        $SemuaBuku = Buku::all();
+
+        $category = $request->get('category', 'SemuaBuku');
+
+        switch ($category) {
+            case 'Fiksi':
+                $books = $BukuFiksi;
+                break;
+            case 'Non Fiksi':
+                $books = $BukuNonFiksi;
+                break;
+            case 'Agama':
+                $books = $BukuAgama;
+                break;
+            case 'Budaya':
+                $books = $BukuBudaya;
+                break;
+            default:
+                $books = $SemuaBuku;
+                break;
+        }
+
+        return view('kategori', compact('category', 'books'));
     }
+
 
     public function wishlist()
     {
--- app/Models/Buku.php
+++ app/Models/Buku.php
@@ -14,6 +14,7 @@ class Buku extends Model
     protected $primaryKey = 'ISBN';
 
     protected $fillable = [
+        'nama_buku',
         'ISBN',
         'kategori_ID',
         'pengarang',
